3c70ce55790346f6a0d4e45ea2251b1bcd02579b
ferzarama <136863921+ferzarama@users.noreply.github.com>
2023-09-11 22:05:42 -0500

    ContactList

---
+++ ListaDeContactos.js
@@ -0,0 +1,74 @@
+const listaDeContactos = [
+  {
+    nombre: "Juan",
+    apellido: "Pérez",
+    telefono: "3102577895",
+    correo: "juan.perez@gmail.com",
+    direccion: "Bogotá",
+  },
+  {
+    nombre: "María",
+    apellido: "González",
+    telefono: "3236587458",
+    correo: "maria.gonzalez@yahoo.com",
+    direccion: "Pasto",
+  },
+  {
+    nombre: "Carlos",
+    apellido: "Rodríguez",
+    telefono: "3132548755",
+    correo: "carlos.rodriguez@hotmail.com",
+    direccion: "Cali",
+    
+  },
+  {
+    nombre: "Pedro",
+    apellido: "Peroni",
+    telefono: "3125444785",
+    correo: "movilventas@gmail.com",
+    direccion: "Medellin",
+  },
+  {
+    nombre: "Augusto",
+    apellido: "Peroni",
+    telefono: "3205877895",
+    correo: "molvilventas@gmail.com",
+    direccion: "Medellin"
+  },
+];
+
+
+const primerContacto = listaDeContactos[0];
+console.log("Nombre: " + primerContacto.nombre);
+console.log("Apellido: " + primerContacto.apellido);
+console.log("Teléfono: " + primerContacto.telefono);
+console.log("Correo: " + primerContacto.correo);
+console.log("dirección: " + primerContacto.direccion);
+
+const segundoContacto = listaDeContactos[0];
+console.log("Nombre: " + segundoContacto.nombre);
+console.log("Apellido: " + segundoContacto.apellido);
+console.log("Teléfono: " + segundoContacto.telefono);
+console.log("Correo: " + segundoContacto.correo);
+console.log("dirección: " + segundoContacto.direccion);
+
+const tercerContacto = listaDeContactos[0];
+console.log("Nombre: " + tercerContacto.nombre);
+console.log("Apellido: " + tercerContacto.apellido);
+console.log("Teléfono: " + tercerContacto.telefono);
+console.log("Correo: " + tercerContacto.correo);
+console.log("dirección: " + tercerContacto.direccion);
+
+const cuartoContacto = listaDeContactos[0];
+console.log("Nombre: " + cuartoContacto.nombre);
+console.log("Apellido: " + cuartoContacto.apellido);
+console.log("Teléfono: " + cuartoContacto.telefono);
+console.log("Correo: " + cuartoContacto.correo);
+console.log("dirección: " + cuartoContacto.direccion);
+
+const quintoContacto = listaDeContactos[0];
+console.log("Nombre " + quintoContacto.nombre);
+console.log("Apellido " + quintoContacto.apellido);
+console.log("Teléfono " + quintoContacto.telefono);
+console.log("Correo " + quintoContacto.correo);
+console.log("Dirección " + quintoContacto.direccion);
